f149af580a9136a5e6ed30fcec004d560d0b0ac0
congnt <congnt24@gmail.com>
2017-09-11 14:08:00 +0700

    minor update

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 let util = require('util');
 let i18n = {
     locales: {}
-    , getVi: (key, ...args) => util.format(i18n.locales['vi'][key], ...args)
-    , getEn: (key, ...args) => util.format(i18n.locales['en'][key], ...args)
+    , getVi: (key, ...args) => util.format(i18n.locales['vi'][key] || key, ...args)
+    , getEn: (key, ...args) => util.format(i18n.locales['en'][key] || key, ...args)
     , get: (lang, key, ...args) => util.format(i18n.locales[lang][key] || key, ...args)
 };
 
 String.prototype.localize = function (lang, ...args) {
-    return i18n.get(lang, this, ...args)
+    return i18n.get(lang, this.toString(), ...args)
 };
 String.prototype.vi = function (...args) {
-    return i18n.get('vi', this, ...args)
+    return i18n.get('vi', this.toString(), ...args)
 };
 String.prototype.en = function (...args) {
-    return i18n.get('en', this, ...args)
+    return i18n.get('en', this.toString(), ...args)
 };
 
 module.exports = i18n;
--- test.js
+++ test.js
@@ -2,6 +2,10 @@ let i18n = require('./index');
 //config
 i18n.locales.vi = require('./vi');
 i18n.locales.en = require('./en');
-console.log(i18n.get('en', 'hello %ssss'));
+
+
+
+console.log(i18n.get('en', 'hello123', 'adasd', 'asdsadsad'));
 console.log(i18n.get('en', 'hello %s', 'cong', 'dz'));
-console.log('hello %s'.localize('vi', 'dai1', 'dai2'));
+console.log('hello %sssss'.localize('vi', 'dai1', 'dai2'));
+console.log('hello %sssss'.vi());
